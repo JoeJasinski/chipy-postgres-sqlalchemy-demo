a800a7d6a933cee358768b4e7e0ba7b059f34291
Joe Jasinski <joe.jasinski@gmail.com>
2024-10-03 00:50:39 -0500

    more work

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,9 @@ RUN pip install pip-tools
 RUN useradd --uid 1000 -ms /bin/bash -d /home/notebook/ -G sudo notebook
 RUN echo "%sudo  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+COPY examples/ /home/examples
 COPY entrypoint.sh /entrypoint.sh
+COPY requirements.in /requirements.in
 COPY requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
---
+++ tasks.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from invoke import task
+
+
+@task
+def build(ctx):
+    ctx.run("docker-compose build", echo=True)
+
+@task(pre=[build])
+def up(ctx):
+    ctx.run("docker-compose up", echo=True)
+
+@task(pre=[build])
+def update_deps(ctx):
+    ret = ctx.run(
+        "docker-compose run web pip-compile --strip-extras /requirements.in", 
+        warn=True, echo=True, )
+    requirements_text = (Path(".") / "requirements.txt")
+    print("Writing output to: ", requirements_text)
+    requirements_text.write_text(ret.stderr)
+
+@task
+def pre(ctx):
+    res = ctx.run("ls -la", echo=True, warn=True)
+    print(res)
